b8471dfd1798c413a9cc9917b8ee3a706789642a
iMac21.5-2012 <61crossroad@gmail.com>
2021-07-03 00:51:34 +0900

    test withdraw & deposit transaction

---
+++ src/test/java/buckpal/account/service/SendMoneyServiceTest.java
@@ -0,0 +1,86 @@
+package buckpal.account.service;
+
+import buckpal.account.application.port.in.SendMoneyCommand;
+import buckpal.account.application.port.out.AccountLock;
+import buckpal.account.application.port.out.LoadAccountPort;
+import buckpal.account.application.port.out.UpdateAccountStatePort;
+import buckpal.account.application.service.MoneyTransferProperties;
+import buckpal.account.application.service.SendMoneyService;
+import buckpal.account.domain.Account;
+import buckpal.account.domain.Account.AccountId;
+import buckpal.account.domain.Money;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+public class SendMoneyServiceTest {
+
+    private final LoadAccountPort loadAccountPort =
+            Mockito.mock(LoadAccountPort.class);
+
+    private final AccountLock accountLock =
+            Mockito.mock(AccountLock.class);
+
+    private final UpdateAccountStatePort updateAccountStatePort =
+            Mockito.mock(UpdateAccountStatePort.class);
+
+    private final SendMoneyService sendMoneyService =
+            new SendMoneyService(loadAccountPort, accountLock, updateAccountStatePort, moneyTransferProperties());
+
+    @Test
+    void transactionSucceeds() {
+        Account sourceAccount = givenSourceAccount();
+        Account targetAccount = givenTargetAccount();
+
+        givenWithdrawalWillSucceed(sourceAccount);
+        givenDepositWillSucceed(targetAccount);
+
+        Money money = Money.of(500L);
+
+        SendMoneyCommand command = new SendMoneyCommand(
+                sourceAccount.getId().get(),
+                targetAccount.getId().get(),
+                money);
+
+        boolean success = sendMoneyService.sendMoney(command);
+
+        assertThat(success).isTrue();
+    }
+
+    private void givenDepositWillSucceed(Account account) {
+        given(account.deposit(any(Money.class), any(AccountId.class)))
+                .willReturn(true);
+    }
+
+    private void givenWithdrawalWillSucceed(Account account) {
+        given(account.withdraw(any(Money.class), any(AccountId.class)))
+                .willReturn(true);
+    }
+
+    private Account givenTargetAccount() {
+        return givenAnAccountWithId(new AccountId(42L));
+    }
+
+    private Account givenSourceAccount() {
+        return givenAnAccountWithId(new AccountId(41L));
+    }
+
+    private Account givenAnAccountWithId(AccountId id) {
+        Account account = Mockito.mock(Account.class);
+        given(account.getId())
+                .willReturn(Optional.of(id));
+        given(loadAccountPort.loadAccount(eq(account.getId().get()), any(LocalDateTime.class)))
+                .willReturn(account);
+
+        return account;
+    }
+
+    private MoneyTransferProperties moneyTransferProperties() {
+        return new MoneyTransferProperties(Money.of(Long.MAX_VALUE));
+    }
+}
