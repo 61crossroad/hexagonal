600e8e80f1851b3c2314da93bd9d490ada33185c
iMac21.5-2012 <61crossroad@gmail.com>
2021-07-02 23:54:19 +0900

    implement test data classes

---
+++ src/test/java/buckpal/account/domain/AccountTest.java
@@ -0,0 +1,12 @@
+package buckpal.account.domain;
+
+import buckpal.account.domain.Account.AccountId;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+
+    @Test
+    void withdrawalSucceeds() {
+        AccountId accountId = new AccountId(1L);
+    }
+}
